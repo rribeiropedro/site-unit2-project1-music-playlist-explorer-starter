8b841e928a82faf9e4d65e01f963b40733557942
Pedro Rabadan Ribeiro <pedrorribeiro@meta.com>
2025-06-04 13:54:44 -0700

    Added exit button to modal and added responsiveness to main page

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -1,1 +1,6 @@
+const modal = document.getElementsByClassName("modal-overlay")[0];
+const span = document.getElementsByClassName("close")[0];
 
+span.onclick = function() {
+   modal.style.display = "none";
+}
