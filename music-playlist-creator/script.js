b6d092b570a25651eee71ed49d8bcd02820a953c
Pedro Rabadan Ribeiro <pedrorribeiro@meta.com>
2025-06-04 17:20:50 -0700

    working dynamic modal except songs list

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -1,47 +1,99 @@
 const modal = document.getElementsByClassName("modal-overlay")[0];
-const span = document.getElementsByClassName("close")[0];
-const playlistList = document.getElementById("playlist-list")
+const modalContent = document.getElementsByClassName("modal-content")[0];
+const playlistList = document.getElementById("playlist-list");
+const cards = document.querySelectorAll(".playlist-cards");
+let span;
 
-span.onclick = function() {
-   modal.style.display = "none";
+function findSongsByName (playlistName) {
+    fetch("./data/data.json")
+        .then(response => response.json())
+        .then(data => {
+            data.forEach(element => {
+                /**  */
+            });
+        })
+        .catch(error => {
+            console.log(error);
+        });
 }
 
-// Populates playlist with data from data.json
-fetch("./data/data.json")
-    .then(response => response.json())
-    .then(data => {
-        data.forEach(element => {
-            console.log(element)
-            let playlist = document.createElement('div');
-            playlist.className = "playlist-cards";
+function loadModal (card) {
+    let image = card.querySelector(".playlist-img");
+    let playlistName = card.querySelector(".playlist-name h1");
+    let creatorName = card.querySelector(".creator-name p");
 
-            let playlistImg = document.createElement('img');
-            playlistImg.className = 'playlist-img';
-            playlistImg.src = '../assets/img/playlist.png';
-            playlist.appendChild(playlistImg);
+    const songs = findSongsByName(playlistName.innerHTML);
 
-            let playlistName = document.createElement('div');
-            playlistName.className = 'playlist-name';
-            let playlistNameH1 = document.createElement('h1');
-            playlistNameH1.innerHTML = element.playlist_name;
-            playlistName.appendChild(playlistNameH1);
-            playlist.appendChild(playlistName);
+    let playlistModalInfo = document.createElement('div');
+    playlistModalInfo.className = "playlist-modal-info";
+    playlistModalInfo.innerHTML = `
+        <img class="playlist-modal-img" src="${image.src}">
+        <div class="playlist-modal-text">
+            <div class="playlist-modal-title">
+                <h2>${playlistName.innerHTML}</h2>
+            </div>
+            <div class="playlist-modal-creator">
+                <h3>${creatorName.innerHTML}</h3>
+            </div>
+        </div>
+        <span class="close">&times;</span>
+    `;
 
-            let creatorName = document.createElement('div');
-            creatorName.className = 'creator-name';
-            let playlistCreatorH1 = document.createElement('h1');
-            playlistCreatorH1.innerHTML = element.playlist_author;
-            creatorName.appendChild(playlistCreatorH1);
-            playlist.appendChild(creatorName);
+    modalContent.appendChild(playlistModalInfo);
+    modal.style.display = "block";
+    span = document.getElementsByClassName("close")[0];
+}
 
-            let likes = document.createElement('div');
-            likes.className = 'like-container'
-            likes.innerHTML = "Likes";
-            playlist.appendChild(likes);
+function createPlaylist (element) {
+    let playlist = document.createElement('div');
+    playlist.className = "playlist-cards";
 
-            playlistList.appendChild(playlist);
+    playlist.innerHTML = `
+        <img class="playlist-img" src=${"./assets/img/playlist.png"}>
+        <div class="playlist-name">
+            <h1>${element.playlist_name}</h1>
+        </div>
+        <div class="creator-name">
+            <p>${element.playlist_author}</p>
+        </div>
+        <div class="like-container">
+            <p>Likes: 0</p>
+        </div>
+    `
+    playlistList.appendChild(playlist);
+}
+
+function loadPlaylists () {
+    fetch("./data/data.json")
+        .then(response => response.json())
+        .then(data => {
+            data.forEach(element => {
+                createPlaylist(element)
+            });
+        })
+        .catch(error => {
+            console.log(error);
         });
+}
+
+function handleClick (event) {
+    if (span) {
+        while (modalContent.firstChild) {
+            modalContent.removeChild(modalContent.firstChild);
+        }
+        span = null;
+        modal.style.display = "none";
+    }
+    const card = event.target.closest('.playlist-cards');
+    if (card) {
+        loadModal(card);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    loadPlaylists();
+
+    document.addEventListener("click", (event) => {
+        handleClick(event);
     })
-    .catch(error => {
-        console.log(error);
-    });
+})
